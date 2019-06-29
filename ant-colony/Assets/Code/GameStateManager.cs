48a348aec91e650c523d1cbfdd320c32e37249be
bradlangel <b.a.langel@gmail.com>
2019-06-29 18:36:36 -0400

    Have a list of initial players
    * Initialize first player in GameStateManager
    * In OnDeath function grab the next playerPrefab in playerPrefabs list
    * TBD: Game over state.

--- ant-colony/Assets/Code/GameStateManager.cs
+++ ant-colony/Assets/Code/GameStateManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameStateManager : MonoBehaviour 
@@ -9,7 +10,10 @@ public class GameStateManager : MonoBehaviour
 
     // TODO: this will probably need to be a list of deadplayerprefabs and their dead positions
     public GameObject deadplayerPrefab;
-    public GameObject playerPrefab;
+
+    // TODO: List of player prefabs to grab from
+
+    public List<GameObject> playerPrefabs;
 
     void Awake() {
         if (_instance == null){
@@ -17,8 +21,32 @@ public class GameStateManager : MonoBehaviour
         }
     }
 
+    // Gets the latest player prefab. If all players have died then it returns null
+    public GameObject getNextPlayer(){
+        if (playerPrefabs.Count != 0) {
+            var playerPrefab = playerPrefabs[0];
+            playerPrefabs.RemoveAt(0);
+            return playerPrefab;
+        } else {
+            // TODO: does such a thing exist as optional in C# to wrap around GameObject for the return type?    
+            return null;
+        }
+    }
+
+    void Start() {
+        // TODO: instantiate the first player
+        var initialPlayer = getNextPlayer();
+        Instantiate(initialPlayer, initialPosition, Quaternion.identity);
+    }
+
     public void OnDeath(Vector3 deathPosition){
         Instantiate(deadplayerPrefab, deathPosition, Quaternion.identity);
-        Instantiate(playerPrefab, initialPosition, Quaternion.identity);
+        var nextPlayer = this.getNextPlayer();
+        if (nextPlayer != null) {
+            Instantiate(nextPlayer, initialPosition, Quaternion.identity);
+        } else {
+            // TODO: GAME OVER
+            Debug.Log("GAME OVER");
+        }
     }
 }
