4927d82ba2a258cd2eee31a0fe999e6c5c5eeb93
zhangoose <hi.zhalex@gmail.com>
2019-06-29 17:28:15 -0400

    Adding dialogue manager,trigger,enum

---
+++ ant-colony/Assets/Code/Dialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    public enum DialogueType {
+        Spawn,
+        Die,
+        Midrun,
+        AtFork,
+        Love,
+    }
+    
+    [TextArea(3, 5)]
+    public string[] sentences;
+}
---
+++ ant-colony/Assets/Code/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation: ");
+    }
+}
